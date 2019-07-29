938bbef3e32c3c55fd004a79aeb44bedb0d8ff40
Graham Fairweather <xotic750@gmail.com>
2019-07-29 22:26:16 +0200

    Rule changes

--- dist/is-plain-object-x.js
+++ dist/is-plain-object-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-27T22:23:05.802Z",
+  "date": "2019-07-29T20:26:11.448Z",
   "describe": "",
   "description": "Tests if a value is a plain object.",
   "file": "is-plain-object-x.js",
